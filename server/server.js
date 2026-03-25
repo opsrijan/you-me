5c4346004cc56ee675a28a5ab16bf7830e3fd2b3
opsrijan <srijankumar2005@gmail.com>
2026-03-25 12:28:09 +0530

    ready for deployment

--- server/server.js
+++ server/server.js
@@ -25,4 +25,7 @@ app.get("/", (req, res) => {
 app.use("/milestones", milestoneRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
